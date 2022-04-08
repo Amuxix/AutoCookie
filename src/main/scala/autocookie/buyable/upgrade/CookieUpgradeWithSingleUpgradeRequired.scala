ef88e7a7f30fb62ab26c7fa721562aad4a63ad32
Amuxix <amuxix@gmail.com>
2022-04-08 18:10:53 +0100

    Fixed some upgrades always having infinite payback

--- src/main/scala/autocookie/buyable/upgrade/CookieUpgradeWithSingleUpgradeRequired.scala
+++ src/main/scala/autocookie/buyable/upgrade/CookieUpgradeWithSingleUpgradeRequired.scala
@@ -1,9 +1,9 @@
 package autocookie.buyable.upgrade
 
 import autocookie.buyable.BuildingRequirement
-import autocookie.buyable.traits.SingleUpgradeRequired
+import autocookie.buyable.traits.{SingleUpgradeRequired, CookiePower}
 
 class CookieUpgradeWithSingleUpgradeRequired(
   override val name: String,
   override protected val upgradeRequirementName: String
-) extends Upgrade with SingleUpgradeRequired
+) extends Upgrade with SingleUpgradeRequired with CookiePower
